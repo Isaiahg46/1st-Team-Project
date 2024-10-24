a59469d3a009c9f3ae698e2235cad87be2433e4b
OnurCulhaci <148915005+OnurCulhaci@users.noreply.github.com>
2024-10-24 14:22:10 -0700

    Add files via upload

---
+++ const quizData = [.js
@@ -0,0 +1,14 @@
+const quizData = [
+    {
+        question: "What function allows you to execute a loop under a specific conditon?"
+        options: ["While", "<div>", "concat()", "cloneNode ()"],
+        answer: "While"
+    },
+    // Continue to add questions - this one is very basic and placed there just as a placeholder.
+];
+
+const startButton = document.getElementById ("start-btn")
+const nextButton = document.getElementById ("next-btn")
+const questionElement = document.getElementbyId ("question")
+const optionsElement = document.getElementbyId ("options")
+    
